a8121875c2ca781c27134b634a6d7c222ec3ba40
Loïc Droz <lk.droz@gmail.com>
2018-10-02 09:19:44 +0200

    Read total # of processes from file.

--- da_proc.c
+++ da_proc.c
@@ -6,6 +6,7 @@
 #include "da_proc.h"
 
 static int wait_for_start = 1;
+static int total_process_number = 0;
 
 static void start(int signum) {
 	wait_for_start = 0;
@@ -33,7 +34,6 @@ int main(int argc, char** argv) {
 	signal(SIGTERM, stop);
 	signal(SIGINT, stop);
 
-
 	// Parse arguments, including membership
 	if (argc < 4) {
 		fprintf(stderr, "ERROR: not enough arguments.\n");
@@ -43,14 +43,34 @@ int main(int argc, char** argv) {
 	int uid = atoi(argv[1]);
 	printf("Process uid = %i\n", uid);
 
-	char * membership_filename = argv[2];
+	const char* membership_filename = argv[2];
 	printf("Membership filename = \"%s\"\n", membership_filename);
 
 	int messages_to_broadcast = atoi(argv[3]);
 	printf("Number of messages to broadcast = %i\n", messages_to_broadcast);
 	if (messages_to_broadcast < 0) {
-		fprintf(stderr, "ERROR: #messages to broadcast cannot be negative.\n")
+		fprintf(stderr, "ERROR: #messages to broadcast cannot be negative.\n");
+		return 1;
 	}
+
+	FILE* membership_file = fopen(membership_filename, "r");
+	if (membership_file == NULL) {
+		fprintf(stderr, "ERROR: Could not open membership file.\n");
+		return 1;
+	}
+
+	// 12 + 3 + 4 + 3 + 5 = 27 -> 32 is enough for one line here.
+	char line[32];
+	// Get total number of processes from the 1st line
+	if (fgets(line, sizeof(line), membership_file) == NULL) {
+		fprintf(stderr, "ERROR: Could not read from membership file.\n");
+		return 1;
+	}
+	total_process_number = atoi(line);
+	printf("Total number of processes = %i\n", total_process_number);
+
+
+	fclose(membership_file);
 
 	// Initialize application
 	// Start listening for incoming UDP packets
