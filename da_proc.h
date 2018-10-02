64425b6ef29273e4123fafae8690bcacefca83f0
Loïc Droz <lk.droz@gmail.com>
2018-10-02 08:22:34 +0200

    Add structure for process.

---
+++ da_proc.h
@@ -0,0 +1,8 @@
+struct da_process {
+    // Unique process identifier for our program
+    unsigned int uid;
+    // 4 bytes, 1 byte per element of the address
+    uint32_t ipv4_addr;
+    // Port number on which the process listens for incoming messages
+    uint16_t port_num;
+}
