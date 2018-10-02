b350b1adb2792da8cc638f369e0f8731b09ab341
Loïc Droz <lk.droz@gmail.com>
2018-10-02 10:12:44 +0200

    Finish parsing the membership file.

--- da_proc.c
+++ da_proc.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <time.h>
+#include <string.h>
 
 #include "da_proc.h"
 
 static int wait_for_start = 1;
 static int total_process_number = 0;
+static struct da_process this_process;
 
 static void start(int signum) {
 	wait_for_start = 0;
@@ -42,16 +44,13 @@ int main(int argc, char** argv) {
 
 	int uid = atoi(argv[1]);
 	printf("Process uid = %i\n", uid);
+	this_process.uid = uid;
 
 	const char* membership_filename = argv[2];
 	printf("Membership filename = \"%s\"\n", membership_filename);
 
 	int messages_to_broadcast = atoi(argv[3]);
 	printf("Number of messages to broadcast = %i\n", messages_to_broadcast);
-	if (messages_to_broadcast < 0) {
-		fprintf(stderr, "ERROR: #messages to broadcast cannot be negative.\n");
-		return 1;
-	}
 
 	FILE* membership_file = fopen(membership_filename, "r");
 	if (membership_file == NULL) {
@@ -64,11 +63,44 @@ int main(int argc, char** argv) {
 	// Get total number of processes from the 1st line
 	if (fgets(line, sizeof(line), membership_file) == NULL) {
 		fprintf(stderr, "ERROR: Could not read from membership file.\n");
+		fclose(membership_file);
 		return 1;
 	}
 	total_process_number = atoi(line);
 	printf("Total number of processes = %i\n", total_process_number);
 
+	int found = 0;
+
+	// Iterate over lines until we find the right line
+	while (fgets(line, sizeof(line), membership_file) != NULL) {
+		// Get line process uid
+		char* puid_str = strtok(line, " ");
+		// Check that this is the line for the process
+		if (puid_str != NULL && atoi(puid_str) == uid) {
+			// Parse IPv4 address and port number
+			const char* ipv4_addr_str = strtok(NULL, " ");
+			const char* port_num_str = strtok(NULL, " ");
+			if (ipv4_addr_str == NULL || port_num_str == NULL ||
+				!inet_aton(ipv4_addr_str, &this_process.ipv4_addr)) {
+				fprintf(stderr, "ERROR: line for process has invalid format.\n");
+				fclose(membership_file);
+				return 1;
+			}
+			this_process.port_num = (unsigned short) atoi(port_num_str);
+
+			printf("Process IPv4 = %s\n", ipv4_addr_str);
+			printf("Process port number = %hu\n", this_process.port_num);
+
+			found = 1;
+			break;
+		}
+	}
+
+	if (!found) {
+		fprintf(stderr, "ERROR: no line matching the provided pid was found.\n");
+		fclose(membership_file);
+		return 1;
+	}
 
 	fclose(membership_file);
 
