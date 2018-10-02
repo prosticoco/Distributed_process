cfc2e1ef467c523281984671dfd77da8081de634
Loïc Droz <lk.droz@gmail.com>
2018-10-02 10:10:50 +0200

    Use appropriate types for ip addr and port number.

--- da_proc.h
+++ da_proc.h
@@ -1,10 +1,13 @@
 #include <stdint.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
 
 struct da_process {
     // Unique process identifier for our program
     int uid;
     // 4 bytes, 1 byte per element of the address
-    uint32_t ipv4_addr;
+    struct in_addr ipv4_addr;
     // Port number on which the process listens for incoming messages
-    uint16_t port_num;
+    in_port_t port_num;
 };
