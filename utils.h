6c120cbe36401e659e4993e07af86580beec2c92
Loïc Droz <lk.droz@gmail.com>
2018-10-08 22:09:14 +0200

    Use new types.

--- utils.h
+++ utils.h
@@ -13,4 +13,6 @@ int initialize_acks_to_send(ack_list_t* acks_to_send, int total_process_number);
 
 void free_acks_to_send(ack_list_t acks_to_send);
 
+bool get_msg_ack(size_t process_index, size_t msg_index);
+
 #endif
