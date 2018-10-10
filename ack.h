1795d713b257fbf0c1e3e092266e99c31008aeea
Loïc Droz <lk.droz@gmail.com>
2018-10-10 14:23:16 +0200

    Add Doxagen comment.

--- ack.h
+++ ack.h
@@ -8,6 +8,14 @@ int init_acks(ack_data_t* data, size_t num_proc, da_process_t* proc_list);
 
 int add_ack(ack_data_t* acks,unsigned int pid,unsigned int msg_no);
 
-int read_ack(ack_data_t* acks,unsigned int pid, unsigned int msg_no);
+/**
+ * @brief Test if a given message sent by a given process was acked.
+ * 
+ * @param acks The ack table to check.
+ * @param pid PID of the process for which we want to check.
+ * @param msg_no Number of the message we want to check.
+ * @return int 1 if message was acked, 0 if not, < 0 in case of an error.   
+ */
+int read_ack(ack_data_t* acks, unsigned int pid, unsigned int msg_no);
 
 int free_acks(ack_data_t* acks);
