c1dc17392397611420346bdc944f39aaa57771ef
Loïc Droz <lk.droz@gmail.com>
2018-10-15 18:45:37 +0200

    Set indentation to 4 spaces.

--- ack.c
+++ ack.c
@@ -32,28 +32,28 @@ static ack_counter_t* get_ack_counter(ack_data_t* acks, unsigned int pid) {
 
 // only call once initializes acks structure and initializes a reader/writer lock
 int init_acks(ack_data_t* data,size_t num_proc,da_process_t* proc_list){
-  int error;
-  if(lock_in_use){
-    return ERROR_ACK_LOCK;
-  }
-  error = pthread_rwlock_init(&ack_lock,&attribute);
-  if(error){
-    return ERROR_LOCK;
-  }
-  error = pthread_rwlockattr_setkind_np(&attribute,PTHREAD_RWLOCK_PREFER_WRITER_NONRECURSIVE_NP);
-  if(error){
-    return ERROR_LOCK;
-  }
-  lock_in_use = 1;
-  data->acks = calloc(num_proc,sizeof(ack_counter_t));
-  if(data->acks == NULL){
-    return ERROR_MEMORY;
-  }
-  for(int i = 0 ; i < num_proc; i++){
-    data->acks[i].counter = 0;
-    data->acks[i].pid = proc_list[i].uid;
-  }
-  return 0;
+    int error;
+    if (lock_in_use) {
+        return ERROR_ACK_LOCK;
+    }
+    error = pthread_rwlock_init(&ack_lock,&attribute);
+    if (error) {
+        return ERROR_LOCK;
+    }
+    error = pthread_rwlockattr_setkind_np(&attribute,PTHREAD_RWLOCK_PREFER_WRITER_NONRECURSIVE_NP);
+    if (error) {
+        return ERROR_LOCK;
+    }
+    lock_in_use = 1;
+    data->acks = calloc(num_proc,sizeof(ack_counter_t));
+    if (data->acks == NULL) {
+        return ERROR_MEMORY;
+    }
+    for (int i = 0 ; i < num_proc; i++) {
+        data->acks[i].counter = 0;
+        data->acks[i].pid = proc_list[i].uid;
+    }
+    return 0;
 }
 
 
