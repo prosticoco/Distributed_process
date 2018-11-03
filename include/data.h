53803207920d82aaff06030b5883ad33bdf31e64
Adrien Prost <adrien.prost@epfl.ch>
2018-11-03 18:53:29 +0100

    created data.h template

---
+++ include/data.h
@@ -0,0 +1,28 @@
+typedef struct{
+
+}fl_data_t;
+
+typedef struct{
+
+}pl_data_t;
+
+typedef struct{
+
+}beb_data_t;
+
+typedef struct{
+
+}urb_data_t;
+
+typedef struct{
+
+}fifo_data_t;
+
+
+typedef struct{
+    fl_data_t fldata;
+    pl_data_t pldata;
+    beb_data_t bebdata;
+    urb_data_t urbdata;
+    fifo_data_t fifodata;    
+}net_data_t
