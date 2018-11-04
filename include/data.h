3283a3510a8474a86dd5015ac7ba5cd045180ed8
Adrien Prost <adrien.prost@epfl.ch>
2018-11-04 14:02:06 +0100

    added the message structures in data.h

--- include/data.h
+++ include/data.h
@@ -22,17 +22,31 @@ typedef struct{
 
 typedef struct{
     unsigned int sequence_num;
+    unsigned int original_sender;
 }fifo_msg_t;
 
+/** every urb message needs the original message that is in beb_msg
+ *  and the pid of the original sender and the current sender
+ */
+typedef struct{
+    fifo_msg_t fifo_msg;
+    unsigned int seen_id;
+    unsigned int no_seen;
+} urb_msg_t;
 
+
+typedef struct {
+    urb_msg_t urb_msg;
+    mid_t mid;
+    unsigned int mtype;
+} msg_t;
 
 
 
 typedef struct{  
     addr_book_t* address_book;
-    size_t self_pid;
+    unsigned int self_pid;
     int fd;
-
     ack_table_t* pl_acks;
     msg_queue_t* task_q;
 }net_data_t;
