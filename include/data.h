b72c4d4cc3c5397938e976f7c473805a2394bdf9
Justinas Sukaitis <justinas.sukaitis>
2018-11-04 16:16:24 +0100

    started fifo

--- include/data.h
+++ include/data.h
@@ -21,18 +21,6 @@ typedef struct {
     uid_table_t table;
 } ack_table_t;
 
-typedef struct {
-    struct sockaddr_in* address;
-    int fd;
-} fl_data_t;
-
-typedef struct {
-
-} pl_data_t;
-
-typedef struct {
-    addr_book_t* address_book;
-} beb_data_t;
 
 typedef struct{
     unsigned int sequence_num;
