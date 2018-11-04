10c0e6368c068e5e9effb1011e303493e46d50cb
Loïc Droz <lk.droz@gmail.com>
2018-11-04 12:50:48 +0100

    Change argument.

--- include/parser.h
+++ include/parser.h
@@ -3,11 +3,12 @@
 #include "data.h"
 
 /**
- * @brief Fill global data structure using arguments and membership file.
+ * @brief Fill global data structure and allocate underlying address book
+ *        using arguments and membership file.
  * 
  * @param argc argc from main.
  * @param argv argv from main.
  * @param data The data structure to fill.
  * @return int 0 in case of success, non-0 otherwise.
  */
-int parse_membership_args(int argc, char** argv, const net_data_t* data);
+int parse_membership_args(int argc, char** argv, net_data_t* data);
