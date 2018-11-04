3b79e79d16006891afcba634e19e980c63bfe86d
Loïc Droz <lk.droz@gmail.com>
2018-11-04 14:38:50 +0100

    Fix some include erros.

--- include/urb.h
+++ include/urb.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "error.h"
 #include "data.h"
 
 //Uniform reliable broadcast
@@ -12,30 +11,26 @@
 //but these 2 are same thing just different name and purpose but same structure
 // table_uid_t delivered_urb, table_uid_t pending
 
-typedef struct{
+typedef struct {
     unsigned int seen;
     unsigned int delivered;
-}urb_entry_t;
+} urb_entry_t;
 
 typedef struct{
     urb_entry_t* values;
     unsigned int no_msgs;
     unsigned int no_process;
     unsigned int total_entries;
-}urb_table_t;
+} urb_table_t;
 
-init_urb_table(urb_table_t* table, unsigned int no_msgs, unsigned int no_process);
-
-
-
-
+int init_urb_table(urb_table_t* table, unsigned int no_msgs, unsigned int no_process);
 
 
 // Main Algorithm methods
 
-int urb_send(urb_data_t* data, urb_msg_t* msg);
+int urb_send(net_data_t* data, urb_msg_t* msg);
 
-int urb_deliver(urb_data_t* data, urb_msg_t* msg);
+int urb_deliver(net_data_t* data, urb_msg_t* msg);
 
-int candeliver(urb_msg_t* msg);
+int candeliver(net_data_t* msg);
 
