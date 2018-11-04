07739b0935635ed5a0415711a292f71ff6091c91
Loïc Droz <lk.droz@gmail.com>
2018-11-04 10:00:18 +0100

    Change deliver_fl() arguments.

--- src/flink.c
+++ src/flink.c
@@ -10,6 +10,7 @@
 #include <unistd.h>
 
 #include "plink.h"
+#include "flink.h"
 
 #include "data.h"
 #include "error.h"
@@ -21,7 +22,7 @@ int send_fl(fl_data_t* data, fl_msg_t* msg) {
     // sends a message to the corresponding address pointed by data
     error = sendto(data->fd,(const char*) msg, sizeof(fl_msg_t),
                     MSG_DONTWAIT,
-                    data-> address,
+                    (const struct sockaddr*) data->address,
                     sizeof(struct sockaddr_in));
     // handles any error due to sendto
     if (error < 0) {
@@ -31,8 +32,7 @@ int send_fl(fl_data_t* data, fl_msg_t* msg) {
 }
 
 //fair loss receive/deliver
-int deliver_fl(struct sockaddr_in* sender, msg_t* msg){
-    int error;
-    error = deliver_pl(struct sockaddr_in* sender, msg_t* msg);
+int deliver_fl(fl_data_t* data, msg_t* msg){
+    int error = deliver_pl(data->address, msg);
     return error;
 }
