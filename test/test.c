98475de905559eaee6fae10a1c6c4e6328971750
Loïc Droz <lk.droz@gmail.com>
2018-11-05 14:25:42 +0100

    Add test.

--- test/test.c
+++ test/test.c
@@ -8,7 +8,7 @@
 /*
     Findings:
     * calling pthread_cancel() on a sleeping thread wakes it up and cancels it.
-    * calling pthread_cancel() on a thread blocked on a mutex, DOES NOT wake it up.
+    * calling pthread_cancel() on a thread blocked on a mutex, DOES NOT wake it up, UNLESS it is set CANCEL ASYNCHRONOUS
     * calling pthread_kill() on a thread blocked on a mutex, KILLS THE WHOLE PROGRAM OH NO
     * same if the thread is just sleeping :(
 */
@@ -17,8 +17,10 @@ pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void *f(void* args) {
     printf("Thread started, try to acquire lock\n");
-    //pthread_mutex_lock(&mutex);
-    usleep(20*S);
+    int dump;
+    pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, &dump);
+    pthread_mutex_lock(&mutex);
+    //usleep(20*S);
     printf("Bye from INSIDE thread\n");
 }
 
@@ -29,7 +31,9 @@ int main(void) {
     pthread_create(&thread, NULL, f, NULL);
     usleep(2*S);
     printf("main() attempts to kill and join thread waiting on lock\n");
-    pthread_kill(thread, SIGKILL);
+    pthread_cancel(thread);
+    pthread_join(thread, NULL);
+    //pthread_kill(thread, SIGKILL);
     printf("Cancel succeeded\n");
     pthread_mutex_unlock(&mutex);
     pthread_mutex_destroy(&mutex);
